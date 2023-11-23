edd4fc11bd84e93942559d74ced245454cd6b872
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-23 22:05:31 +0530

    Update Ganesh.py

--- Ganesh.py
+++ Ganesh.py
@@ -4,9 +4,11 @@ import time
 st.header("Welcome To App")
 g=st.button(label="Run")
 tm=st.empty
+i=0
 with st.empty():
-    for seconds in range(10):
-        st.write(f"⏳ {datetime.datetime.now()}")
+    while True:
+        i=i+1
+        st.write(f"{i} ⏳ {datetime.datetime.now()}")
         time.sleep(1)
     st.write("✔️ 1 minute over!")
 if g:
