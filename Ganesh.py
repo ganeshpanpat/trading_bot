14582431707818ec6bc048f15d30b85f131602d8
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-23 20:53:07 +0530

    Update Ganesh.py

--- Ganesh.py
+++ Ganesh.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import datetime
+import time
 st.header("Welcome To App")
 g=st.button(label="Run")
+tm=st.empty()
 if g:
   st.write('hi')
   st.write(datetime.datetime.now())
+  for i in range(0,10):
+    tm.text=datetime.datetime.now()
+    time.sleep(1)
+    
   
