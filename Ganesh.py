1c3d9f6982f23e75e15a550c3f4f595d920359a5
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-27 14:00:57 +0530

    Update Ganesh.py

--- Ganesh.py
+++ Ganesh.py
@@ -9,4 +9,4 @@ if 'user_name' not in st.session_state or st.session_state['user_name']=='Guest'
     st.session_state['login_time']='Guest'
     st.session_state['last_update']=datetime.datetime.now()-datetime.timedelta(minutes=5)
 def get_dateframe():
-    st.session
+    st.session['last_update']=datetime.datetime.now()
